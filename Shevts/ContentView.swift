fab615d196fb2f08622342eafa76c74342a9c8ff
vovik_saharok <vladimireally@gmail.com>
2025-03-03 11:31:35 +0300

    Initial Commit

---
+++ Shevts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shevts
+//
+//  Created by User on 03.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shevts/ShevtsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShevtsApp.swift
+//  Shevts
+//
+//  Created by User on 03.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct ShevtsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
